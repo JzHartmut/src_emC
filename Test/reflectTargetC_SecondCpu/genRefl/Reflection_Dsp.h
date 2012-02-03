ee6c771b35998bfdc7f09b9e7f6d7917961d4c54
Hartmut Schorrig <hartmut@vishia.org>
2012-02-04 00:00:00 +0100

    addes some more files, generated reflection for test, dep-files from MsC6

---
+++ Test/reflectTargetC_SecondCpu/genRefl/Reflection_Dsp.h
@@ -0,0 +1,21 @@
+
+#ifndef __Reflection_Dsp_h__
+
+#define __Reflection_Dsp_h__
+
+#include "Jc/ReflectionJc.h"
+extern_C const ClassJc reflection_SimpleDataDsp;
+#define REFLECTION_SimpleDataDsp &reflection_SimpleDataDsp
+extern_C const ClassJc reflection_ExampleArrayElement;
+#define REFLECTION_ExampleArrayElement &reflection_ExampleArrayElement
+extern_C const ClassJc reflection_ExampleIntern;
+#define REFLECTION_ExampleIntern &reflection_ExampleIntern
+extern_C const ClassJc reflection_BitFieldTest;
+#define REFLECTION_BitFieldTest &reflection_BitFieldTest
+extern_C const ClassJc reflection_ExampleData;
+#define REFLECTION_ExampleData &reflection_ExampleData
+extern_C const ClassJc reflection_ExampleUnion;
+#define REFLECTION_ExampleUnion &reflection_ExampleUnion
+extern_C const ClassJc reflection_MainSecondCpu;
+#define REFLECTION_MainSecondCpu &reflection_MainSecondCpu
+#endif // ___h__
---
+++ Test/reflectTargetCpp/genRefl/Reflection_TestReflectTarget.h
@@ -0,0 +1,13 @@
+
+#ifndef __Reflection_TestReflectTarget_h__
+
+#define __Reflection_TestReflectTarget_h__
+
+#include "Jc/ReflectionJc.h"
+extern_C const ClassJc reflection_SubData;
+#define REFLECTION_SubData &reflection_SubData
+extern_C const ClassJc reflection_Main;
+#define REFLECTION_Main &reflection_Main
+extern_C const ClassJc reflection_Data2;
+#define REFLECTION_Data2 &reflection_Data2
+#endif // ___h__
