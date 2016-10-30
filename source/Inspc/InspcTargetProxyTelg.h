3a1cdcb93a5ae08e1c70b20eaff14e1c671f0dc6
Hartmut Schorrig <hartmut@vishia.org>
2016-10-30 20:18:34 +0100

    gardening InspcTargetProxy

--- source/Inspc/InspcTargetProxyTelg.h
+++ source/Inspc/InspcTargetProxyTelg.h
@@ -82,36 +82,6 @@ typedef enum Cmd_InspcTargetProxyTelg_t
 
 
 
-/**This struct describes the common head of any telegram for inspector communication. 16 Byte. */
-typedef struct InspcTelgHead_Inspc_t
-{
-  /**Nr of bytes of this telegram. */
-  int16BigEndian nrofBytes;
-
-  /**The command is a version identificator too. A new version uses new command constants. 
-   * That is used as BOM (Byte Order Mark) too: 
-   * * A big endian command sets its bit15 to 0 anyway. It means the bit 7 in the first byte is 0.
-   * * A little endian command sets its bit 7 to 1. It means the bit 7 in the first byte is 1. 
-   */
-  int16BigEndian cmdTelg;
-
-  #define mLittleEndianCmdTelg_Inspc 0x0080;
-
-  /**A number to support an encryption. For future extensions. */
-  int32BigEndian encryption;
-
-  /**The sequence number of this telegram. The request and the answer have the same sequence number. */
-  int32BigEndian seqnr;
-
-  /**A answer number if the answer uses more as one telegram. */
-  int16BigEndian answernr;
-
-  /**Distinction if more as one service is used with the same port of communication. */ 
-  int16BigEndian entrant;
-
-
-} InspcTelgHead_Inspc_s;
-
 
 
 
@@ -119,13 +89,15 @@ typedef struct InspcTelgHead_Inspc_t
  * The first 2 bytes should be the length, after them 2 bytes are the command.
  * All other bytes are special defined here.
  */
-typedef struct ItemProxy2Target_Inspc_t
+typedef struct TelgProxy2Target_Inspc_t
 {
   /**The length is always 12 because that is the fix length. */
   int16BigEndian length;
   
   /**One of the Cmd_InspcTargetProxyTelg_e. */
   int16BigEndian cmd;
+
+  int32BigEndian seqnr;
 
   /**First info. */
   int32BigEndian address;
@@ -134,47 +106,24 @@ typedef struct ItemProxy2Target_Inspc_t
   int32BigEndian value;
 
 
-} ItemProxy2Target_Inspc_s;
+} TelgProxy2Target_Inspc_s;
 
 
 /**This struct describes one item in the telegram from the target. */
-typedef struct ItemTarget2Proxy_Inspc_s_t
+typedef struct TelgTarget2Proxy_Inspc_s_t
 {
   /**The length is always 8 because that is the fix length. */
   int16BigEndian length;
   
   int16BigEndian cmd;
 
+  int32BigEndian seqnr;
+
   int32BigEndian retValue;
 
-} ItemTarget2Proxy_Inspc_s;
-
-
-
-/**This struct describes the telegram from the proxy to the target to send commands. */
-typedef struct TelgProxy2Target_Inspc_t
-{
-  InspcTelgHead_Inspc_s head;
-
-  /**Up to 100 items with commands from Proxy to the target, often only 1 used. 
-   * The max length of a telegram is 1216 with them. 
-   */
-  ItemProxy2Target_Inspc_s items[100];
-
-} TelgProxy2Target_Inspc_s;
-
-
-/**This struct describes the telegram from the target to the proxy to send answers. */
-typedef struct TelgTarget2Proxy_Inspc_t
-{
-  InspcTelgHead_Inspc_s head;
-
-  /**Up to 100 items with answers from target to the proxy, often only 1 used. 
-   * The max length of a telegram is 816 with them. 
-   */
-  ItemTarget2Proxy_Inspc_s items[100];
-
 } TelgTarget2Proxy_Inspc_s;
+
+
 
 
 /**This const array is defined by the reflection generator in the ReflOffset.c file. */
@@ -196,8 +145,8 @@ extern_C const int32* reflectionOffsetArrays[];
  * @param reflectionOffsetArrays pointer to the 'Array of all offsets' from the generated ...ReflOffset...c-File.
  * @return for set and get operations the current value, gotten or after set. It is a int image of a float value on get/set double and float.  
  */
-int32 processInspcCommand(Cmd_InspcTargetProxyTelg_e const cmd, void const* const address, int32 const inputVal, void const* const mainData
-, int32 const* const reflectionOffset_MainData, int32 const* const* const reflectionOffsetArrays);
+int32 processInspcCmdOnTarget_Inspc(Cmd_InspcTargetProxyTelg_e const cmd, int32 address, int32 inputVal, void const* mainData
+, int32 const* reflectionOffset_MainData, int32 const* const* reflectionOffsetArrays);
 
 
 #endif //__InspcTargetProxyTelg_h__
